b289211cde117b5ab51eaed8be4f4d288ed7b682
decanoe <deca.noe49@gmail.com>
2022-02-03 15:07:49 +0100

    python classes

---
+++ pythonClass/comment.py
@@ -0,0 +1,5 @@
+class Comment:
+    def __init__(self, post, user, content):
+        self.post = post
+        self.user = user
+        self.content = content
---
+++ pythonClass/post.py
@@ -0,0 +1,5 @@
+class Post:
+    def __init__(self, author, title, content):
+        self.author = author
+        self.title = title
+        self.content = content
