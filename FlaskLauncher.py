93efb6bb46d7a7ce9c6cb2c0183ca6da9bf501c1
Neww3r <erwan.legrand49@gmail.com>
2022-02-04 12:06:17 +0100

    commit

---
+++ FlaskLauncher.py
@@ -0,0 +1,46 @@
+import sqlite3
+from sqlite3 import Error
+from tracemalloc import start
+from django.shortcuts import render
+from flask import *
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	print("launched")
+	return render_template("index.html")
+
+if __name__ == "__main__":
+	app.run()
+
+# tests
+
+# blueprint for auth routes in our app
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
+
+# blueprint for non-auth parts of app
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+
+def startConnection(path):
+    connect = None
+    try:
+        connect = sqlite3.connect(path)
+    except Error as e:
+        print(e)
+
+    return connect
+
+connection = startConnection("database.db")
+
+cur = connection.cursor()
+#cur.execute('CREATE TABLE posts ("id_post" INTEGER, "autor" INTEGER, "title" TEXT NOT NULL, "content" TEXT NOT NULL, PRIMARY KEY("id_post"), FOREIGN KEY("autor") REFERENCES "users"("id"))')
+cur.execute('CREATE TABLE comments ("id_comment" INTEGER, "post" INTEGER, "user" INTEGER, "content" TEXT NOT NULL, PRIMARY KEY("id_comment"), FOREIGN KEY("user") REFERENCES "users"("id"), FOREIGN KEY("post") REFERENCES "posts"("id_post"))')
+#cur.execute('CREATE TABLE users ("id" INTEGER, "pseudo" TEXT NOT NULL, "password" TEXT NOT NULL, "email" TEXT NOT NULL, "age" INTEGER NOT NULL, PRIMARY KEY("id"))')
+#cur.execute('DROP TABLE users')
+'''
+cur.execute('SELECT * FROM users')
+
+for row in cur.fetchall():
+	print(row)'''
