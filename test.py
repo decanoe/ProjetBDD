cae2a40b4c8628ef3cc88d1aabb68748e4f061be
Neww3r <erwan.legrand49@gmail.com>
2022-02-23 16:56:44 +0100

    commit

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ connection = startConnection("database.db")
 cur = connection.cursor()
 req = "INSERT INTO users (login,password,email,age) VALUES ('erwan2511','test','erwan.legrand@gmail.com',17)"
 cur.execute(req)
+req = "INSERT INTO users (login,password,email,age) VALUES ('totoro49','a','@rnaque.i',16)"
+cur.execute(req)
 cur.execute('select login from users')
 list_login = cur.fetchone()
 print(list_login)
+connection.commit()
+connection.close()
