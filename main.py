897c84e27c163117d9e8c93b34ff63f71574698a
Neww3r <erwan.legrand49@gmail.com>
2022-03-01 08:56:53 +0100

    commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from app import startConnection
 from pythonClass.post import Post
+from pythonClass.comment import Comment
 
 main = Blueprint('main', __name__)
 
@@ -21,8 +22,8 @@ def forum():
     cursor = connection.cursor()
 
     list_post = []
-    for post in cursor.execute("SELECT author, title, content FROM posts").fetchall():
-        list_post.append(Post(post[0], post[1], post[2]))
+    for post in cursor.execute("SELECT * FROM posts").fetchall():
+        list_post.append(Post(post[0], post[1], post[2], post[3]))
     
     from auth import connectedAs as user
     return render_template("forum.html", list_post = list_post, connectedAs = user)
@@ -38,7 +39,18 @@ def resetData():
     from auth import connectedAs as user
     return render_template("index.html", message = "La table users a été reset", connectedAs = user)
 
-@main.route('/post/<int:post_id>')
-def show_post(post_id):
+@main.route('/post/<int:id_post>')
+def show_post(id_post):
+    connection = startConnection("database.db")
+    cursor = connection.cursor()
+    result = cursor.execute("SELECT * FROM posts WHERE id_post="+str(id_post)+";").fetchone()
+    post = Post(result[0], result[1], result[2], result[3])
     
-    return render_template("post.html", post = )
+    result = cursor.execute("SELECT post, user, content FROM comments WHERE post="+str(id_post)+";").fetchall()
+    list_comment = []
+
+    for comment in result:
+        list_comment.append(Comment(comment[0],comment[1],comment[2]))
+
+    from auth import connectedAs as user
+    return render_template("post.html", post = post, list_comment = list_comment, connectedAs = user)
