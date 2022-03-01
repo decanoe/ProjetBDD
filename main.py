5362371fd49a9968538f1c3383e768af15d4b686
Neww3r <erwan.legrand49@gmail.com>
2022-03-01 09:56:54 +0100

    commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request
 from app import startConnection
 from pythonClass.post import Post
 from pythonClass.comment import Comment
@@ -45,8 +45,7 @@ def show_post(id_post):
     cursor = connection.cursor()
     result = cursor.execute("SELECT * FROM posts WHERE id_post="+str(id_post)+";").fetchone()
     post = Post(result[0], result[1], result[2], result[3])
-    
-    result = cursor.execute("SELECT post, user, content FROM comments WHERE post="+str(id_post)+";").fetchall()
+    result = cursor.execute("SELECT post, login, content FROM comments JOIN users ON users.id = comments.user WHERE post="+str(id_post)+";").fetchall()
     list_comment = []
 
     for comment in result:
@@ -54,3 +53,15 @@ def show_post(id_post):
 
     from auth import connectedAs as user
     return render_template("post.html", post = post, list_comment = list_comment, connectedAs = user)
+
+@main.route("/comment", methods=["POST"])
+def comment():
+    from auth import connectedAs as user
+    connection = startConnection("database.db")
+    cursor = connection.cursor()
+    id_post = request.form['id_post']
+    content = request.form['content']
+    cursor.execute("INSERT INTO comments (post,user,content) VALUES("+str(id_post)+","+str(user.id)+",'"+content+"')")
+    connection.commit()
+    connection.close()
+    return redirect("/post/"+str(id_post))
