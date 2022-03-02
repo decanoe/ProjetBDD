d5e9192cea533ba8ba046bf9ea79d73cd704b054
decanoe <deca.noe49@gmail.com>
2022-03-02 16:49:58 +0100

    commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, request
 from app import startConnection
 from pythonClass.post import Post
 from pythonClass.comment import Comment
+from pythonClass.user import User
 
 main = Blueprint('main', __name__)
 
@@ -22,7 +23,7 @@ def forum():
     cursor = connection.cursor()
 
     list_post = []
-    for post in cursor.execute("SELECT * FROM posts").fetchall():
+    for post in cursor.execute("SELECT id_post, login, title, content FROM posts JOIN users ON users.id = posts.author").fetchall():
         list_post.append(Post(post[0], post[1], post[2], post[3]))
     
     from auth import connectedAs as user
@@ -43,11 +44,11 @@ def resetData():
 def show_post(id_post):
     connection = startConnection("database.db")
     cursor = connection.cursor()
-    result = cursor.execute("SELECT * FROM posts WHERE id_post="+str(id_post)+";").fetchone()
+    result = cursor.execute("SELECT id_post, login, title, content FROM posts JOIN users ON users.id = posts.author WHERE id_post="+str(id_post)+";").fetchone()
     post = Post(result[0], result[1], result[2], result[3])
+
     result = cursor.execute("SELECT post, login, content FROM comments JOIN users ON users.id = comments.user WHERE post="+str(id_post)+";").fetchall()
     list_comment = []
-
     for comment in result:
         list_comment.append(Comment(comment[0],comment[1],comment[2]))
 
