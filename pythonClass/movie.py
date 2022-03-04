46711c66cf085547823799a9c3e2b18eae639c17
BuildTools <unconfigured@null.spigotmc.org>
2022-03-04 23:41:29 +0100

    commit

--- pythonClass/movie.py
+++ pythonClass/movie.py
@@ -1,7 +1,11 @@
 class Movie:
-    def __init__(self,id, author, title, content):
+    def __init__(self,id, title, realisator, date, duration, image_path, genres, resum, resum_author):
         self.id = id
-        
-        self.author = author
         self.title = title
-        self.content = content
+        self.realisator = realisator
+        self.date = date
+        self.duration = duration
+        self.image_path = image_path
+        self.genres = genres
+        self.resum = resum
+        self.resum_author = resum_author
--- test3.py
+++ test3.py
@@ -1,3 +1,11 @@
-char = "Bonjour c'est moi"
-test = char.replace("'","''")
-print(test)
+from multiprocessing import connection
+import _sqlite3
+from app import startConnection
+
+connection = startConnection("database.db")
+cur = connection.cursor()
+
+req = "DELETE FROM comments"
+cur.execute(req)
+connection.commit()
+connection.close()
