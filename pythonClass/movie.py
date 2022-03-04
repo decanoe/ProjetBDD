109ec2316cf6f7afa9f13d4843b979e6830e3537
decanoe <deca.noe49@gmail.com>
2022-03-04 16:47:25 +0100

    commit

---
+++ pythonClass/movie.py
@@ -0,0 +1,7 @@
+class Movie:
+    def __init__(self,id, author, title, content):
+        self.id = id
+        
+        self.author = author
+        self.title = title
+        self.content = content
