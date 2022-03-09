b87756132e84d9b7d8db9d1daf032dcb10506576
Neww3r <erwan.legrand49@gmail.com>
2022-03-09 15:54:33 +0100

    commit

--- test2.py
+++ test2.py
@@ -4,6 +4,6 @@ from app import startConnection
 connection = startConnection("database.db")
 cur = connection.cursor()
 
-req = "DELETE FROM movies WHERE id = 24 OR id = 25"
+req = "DELETE FROM movies WHERE id = 27 OR id = 28"
 cur.execute(req)
 connection.commit()
