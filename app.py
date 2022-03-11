65b31458968bd856e42bfa3607ecf116c7e960fa
Neww3r <erwan.legrand49@gmail.com>
2022-03-11 11:27:39 +0100

    commit

--- app.py
+++ app.py
@@ -8,11 +8,9 @@ def create_app():
     app = Flask(__name__)
     app.config['UPLOAD_FOLDER'] = 'static/movies'
 
-    # blueprint for auth routes in our app
     from auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-    # blueprint for non-auth parts of app
     from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
